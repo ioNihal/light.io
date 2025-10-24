56887706f223f4f6d5dc0be7f7ff247083b3bb90
Nihal K <nihal04x@gmail.com>
2025-10-24 11:13:58 +0530

    Color blindness updated

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,23 @@
 import { useNavigate } from 'react-router-dom';
 import styles from './Header.module.css';
-import { GoHeart } from 'react-icons/go';
+import { GoHeart, GoHeartFill } from 'react-icons/go';
+import { useState } from 'react';
 
 export default function Header({ toolName }) {
+    const [liked, toggleLiked] = useState(false);
 
     const navigate = useNavigate();
+
+    const handleLikeClick = () => {
+        toggleLiked((prev) => !prev)
+    }
 
 
     return (
         <div className={styles.header}>
             <h1>{toolName}</h1>
             <div className={styles.btnGroup}>
-                <span className={styles.likeBtn}><GoHeart size={"1.5rem"} /></span>
+                <span className={styles.likeBtn} onClick={handleLikeClick}>{liked ? <GoHeartFill size={"1.5rem"} /> : <GoHeart size={"1.5rem"} />}</span>
                 <button>Menu</button>
                 <button onClick={() => navigate('/')}>Back</button>
             </div>
